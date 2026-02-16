a66a764ff6a6360e8dcac62c434086b444c888e0
Riegooo <christiandanielcagas0@gmail.com>
2026-02-16 09:17:49 +0800

    adjust

--- src/constant/order-status.js
+++ src/constant/order-status.js
@@ -1,14 +1,9 @@
 
-
 function randomStatus(max) {
     return Math.floor(Math.random() * max)
 };
-
 const statusList = ["Pending", "Processing", "Completed", "Cancelled"]
-
 let myStatus = statusList[randomStatus(3)]
-
-
 
 module.exports = {
     STATUS: myStatus
